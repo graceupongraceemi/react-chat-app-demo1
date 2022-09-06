8e96b09e481cdf6c53810868defe89c500a85ec4
Emiko Esther S <emiko.esther.self@gmail.com>
2022-09-06 14:07:37 -0700

    chat, message, sendmessage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 
 import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Chat from './components/Chat';
 
 const style = {
   appContainer: `max-w-[728px] mx-auto text-center`,
@@ -18,7 +19,8 @@ function App() {
       <section className={style.sectionContainer}>
         {/* Navbar */}
         <Navbar />
-        {/* Chat component */}
+        {user ? <Chat /> : null}
+        <Chat />
       </section>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -20,5 +20,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export default app;
